208c7d6e4ae98d78098b531ba359a7b2c752cc1b
pekpytxd <akatev58@gmail.com>
2024-08-01 09:45:22 +0300

    750

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ headers = {
 }
 request_body = {'tapsCount': 7}
 count = 0
-while (count <= 700):
+while (count <= 750):
     res = requests.post(URL, json=request_body, headers=headers)
     print(res)
     count += 1
