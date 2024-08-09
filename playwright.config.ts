c8fabf9f73305d8aab98c5554aa1e09f15b19d93
pekpytxd <akatev58@gmail.com>
2024-08-09 18:03:16 +0300

    no chann

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
 });
